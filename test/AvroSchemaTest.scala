9c2b6e00d8f26d35af5c3a23b54b0b2f4120eec1
Quinn Slack <qslack@qslack.com>
2013-02-02 10:04:40 -0800

    RecordWith2Strings

--- test/AvroSchemaTest.scala
+++ test/AvroSchemaTest.scala
@@ -5,6 +5,7 @@ import org.scalatest.matchers.ShouldMatchers
 
 case class EmptyRecord()
 case class RecordWithString(a: String)
+case class RecordWith2Strings(a: String, b: String)
 
 class AvroSchemaTest extends FunSpec with ShouldMatchers {
   describe("EmptyRecord") {
@@ -16,6 +17,12 @@ class AvroSchemaTest extends FunSpec with ShouldMatchers {
   describe("RecordWithString") {
     it("should generate the corresponding Avro schema") {
       AvroSchema.from[RecordWithString].toString should be ("""{"type":"record","name":"RecordWithString","namespace":"org.apache.avro.scala.schema","fields":[{"name":"a","type":"string"}]}""")
+    }
+  }
+
+  describe("RecordWith2Strings") {
+    it("should generate the corresponding Avro schema") {
+      AvroSchema.from[RecordWith2Strings].toString should be ("""{"type":"record","name":"RecordWith2Strings","namespace":"org.apache.avro.scala.schema","fields":[{"name":"a","type":"string"},{"name":"b","type":"string"}]}""")
     }
   }
 }
